b0c142778eee98c07c9335ea13bab1154c1b208f
Rohit-Gupta11 <grohit7711@gmail.com>
2023-11-05 13:56:03 +0530

    project done

---
+++ src/assets/index.ts
@@ -0,0 +1,16 @@
+import link from './link.svg'
+import loader from './loader.svg'
+import copy from './copy.svg'
+import logo from './logo.svg'
+import tick from './tick.svg'
+import grid from './grid.svg'
+
+
+export {
+    link,
+    loader,
+    copy,
+    logo,
+    tick,
+    grid
+}
